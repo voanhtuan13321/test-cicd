44bf35adeb7c1c292e171e18c140e45f25a7440c
Anh Tuan <84724847+voanhtuan13321@users.noreply.github.com>
2025-08-07 20:55:51 +0700

    init project

---
+++ src/main/java/com/example/demo_cicd/DemoCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_cicd/DemoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
